8c4d771792acc5e26dfebff4a1b58ad3affa5335
sjctrags <sjctrags@yahoo.com>
2022-07-21 15:09:57 +0800

    Change send_book to reserve_book.
    Fixing bugs.

--- ch04/student_mgt/controllers/reservations.py
+++ ch04/student_mgt/controllers/reservations.py
@@ -4,25 +4,18 @@ from pydantic import BaseModel
 from fastapi.encoders import jsonable_encoder
 from json import dumps
 import httpx
+from models.request.library import BookIssuanceReq
 
 router = APIRouter()
-
-class Book(BaseModel): 
-    isbn:str
-    title:str 
-    author:str 
-    qty:int 
-    price:float
 
 @router.get('/access/book')
 def access_book(): 
    with httpx.Client() as client:
-    response = client.get("http://localhost:8000/library/book")
+    response = client.get("http://localhost:8000/ch04/library/book/list")
     return response.json()
 
-@router.get('/send/book')
-def send_book(): 
+@router.get('/reserve/book')
+def reserve_book(book:BookIssuanceReq): 
   with httpx.Client() as client:
-     book1 = Book(isbn='1902', title='Hello', author='xxx', qty=10, price=5000.5)
-     response = client.post("http://localhost:8000/library/return/book",  data={"book":dumps(jsonable_encoder(book1))})
+     response = client.post("http://localhost:8000/ch04/library/book/issuance",  data={"book":dumps(jsonable_encoder(book))})
      return response.content 
