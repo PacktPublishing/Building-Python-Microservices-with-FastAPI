8018cb93ad129e0036fb049a7c4a0e601c5c06e3
sjctrags <sjctrags@yahoo.com>
2022-08-03 04:05:14 +0800

    Fixed some codes.
    Added Security injection to APIs and cleaned login APIs.

--- ch07h/api/admin.py
+++ ch07h/api/admin.py
@@ -1,20 +1,20 @@
-from fastapi import APIRouter, Depends
-from fastapi.encoders import jsonable_encoder
+from fastapi import APIRouter, Depends, Security
 from fastapi.responses import JSONResponse
 
 from sqlalchemy.orm import Session
-from db_config.sqlalchemy_connect import SessionFactory
 from models.data.sqlalchemy_models import Signup
 from repository.signup import SignupRepository
 from models.request.signup import SignupReq
 from typing import List
 from db_config.sqlalchemy_connect import sess_db
+from models.data.sqlalchemy_models import Signup, Login
+from security.secure import  get_current_user
 
 router = APIRouter()
         
 
 @router.post("/signup/add")
-def add_signup(req: SignupReq, sess:Session = Depends(sess_db)):
+def add_signup(req: SignupReq, sess:Session = Depends(sess_db), current_user:Login = Security(get_current_user)):
     repo:SignupRepository = SignupRepository(sess)
     signup = Signup(password= req.password, username=req.username,id=req.id)
     result = repo.insert_signup(signup)
@@ -24,13 +24,13 @@ def add_signup(req: SignupReq, sess:Session = Depends(sess_db)):
         return JSONResponse(content={'message':'create signup problem encountered'}, status_code=500)
 
 @router.get("/signup/list", response_model=List[SignupReq])
-def list_signup(sess:Session = Depends(sess_db)):
+def list_signup(sess:Session = Depends(sess_db), current_user:Login = Security(get_current_user)):
     repo:SignupRepository = SignupRepository(sess)
     result = repo.get_all_signup()
     return result
 
 @router.patch("/signup/update")
-def update_signup(id:int, req: SignupReq, sess:Session = Depends(sess_db) ):
+def update_signup(id:int, req: SignupReq, sess:Session = Depends(sess_db), current_user:Login = Security(get_current_user) ):
     signup_dict = req.dict(exclude_unset=True)
     repo:SignupRepository = SignupRepository(sess)
     result = repo.update_signup(id, signup_dict )
@@ -41,7 +41,7 @@ def update_signup(id:int, req: SignupReq, sess:Session = Depends(sess_db) ):
     
 
 @router.delete("/signup/delete")
-def delete_signup(id:int, sess:Session = Depends(sess_db) ):
+def delete_signup(id:int, sess:Session = Depends(sess_db), current_user:Login = Security(get_current_user) ):
     repo:SignupRepository = SignupRepository(sess)
     result = repo.delete_signup(id )
     if result: 
@@ -50,7 +50,7 @@ def delete_signup(id:int, sess:Session = Depends(sess_db) ):
         return JSONResponse(content={'message':'update profile error'}, status_code=500)
     
 @router.get("/signup/list/{id}", response_model=SignupReq)
-def get_signup(id:int, sess:Session = Depends(sess_db)): 
+def get_signup(id:int, sess:Session = Depends(sess_db), current_user:Login = Security(get_current_user)): 
     repo:SignupRepository = SignupRepository(sess)
     result = repo.get_signup(id)
     return result
--- ch07h/api/login.py
+++ ch07h/api/login.py
@@ -1,26 +1,8 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Security
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse, RedirectResponse, Response
-from fastapi.requests import Request
-from sqlalchemy.orm import Session
-from db_config.sqlalchemy_connect import SessionFactory
-from db_config.sqlalchemy_connect import sess_db
-from models.data.sqlalchemy_models import Signup, Login
-from repository.signup import SignupRepository
-from repository.login import LoginRepository
+from fastapi import APIRouter, HTTPException, Security
+from models.data.sqlalchemy_models import Login
 
-from passlib.context import CryptContext
-from fastapi.security import HTTPBasicCredentials
-
-from fastapi.security import HTTPBearer
-
-from datetime import date, timedelta
 from security.secure import  get_current_user
 router = APIRouter()
-
-# Scheme for the Authorization header
-# 👈 new code
-
 
 @router.get("/private")
 def private(current_user:Login = Security(get_current_user)):
