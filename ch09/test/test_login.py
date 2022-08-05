7b56dba7ab89d4e08ed4fedeb4a7abc733768d95
sjctrags <sjctrags@yahoo.com>
2022-08-05 16:34:13 +0800

    Fixed bugs on database mocking.
    Finalize database mocking.

--- ch09/test/test_login.py
+++ ch09/test/test_login.py
@@ -1,33 +1,26 @@
 from fastapi.testclient import TestClient
 from models.data.orrs import Login
 from main import app
-import pytest
 
 from util.auth_session import get_current_user
+from config.db import create_db_engine
 from odmantic import AIOEngine
 from motor.motor_asyncio import AsyncIOMotorClient
 
 client = TestClient(app)
 
-
-@pytest.fixture(scope="module")
-def db_session():
-    global engine
+def db_connect():
     client_od = AsyncIOMotorClient(f"mongodb://localhost:27017/")
-    engine = AIOEngine(motor_client=client_od, database="orrs")
-    yield engine  # use the session in tests.
-    client_od.close()
-    
-def get_engine():
+    engine = AIOEngine(motor_client=client_od, database="orrs_test")
     return engine
 
 async def get_user():
     return Login(**{"username": "sjctrags", "login_id": 101,  "password":"sjctrags", "passphrase": None, "profile": None})
 
 app.dependency_overrides[get_current_user] =  get_user
-app.dependency_overrides[get_current_user] = get_engine()
+app.dependency_overrides[create_db_engine] = db_connect
 
 def test_list_login():
     response = client.get("/ch09/login/list/all")
-    assert response.status_code == 200
+    assert response.status_code == 201
     
