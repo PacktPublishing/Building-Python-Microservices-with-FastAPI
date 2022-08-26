f1f382ccc1b717f0a5890e85b736e9b884910388
sjctrags <sjctrags@yahoo.com>
2022-08-27 00:07:31 +0800

    Finalize codes
    Finalize codes and fixed some bugs.

--- ch05b/main.py
+++ ch05b/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-from api import login, attendance
+from api import login, attendance, gym
 
 app = FastAPI()
 app.include_router(login.router, prefix='/ch05')
 app.include_router(attendance.router, prefix='/ch05')
+app.include_router(gym.router, prefix='/ch05')
 
