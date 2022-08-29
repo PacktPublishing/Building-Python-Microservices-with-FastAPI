1c7aeac3c358aed81b46705095bb0d483954252e
sjctrags <sjctrags@yahoo.com>
2022-08-30 07:53:39 +0800

    finalize codes
    Finalized codes

--- ch07b/api/login.py
+++ ch07b/api/login.py
@@ -1,22 +1,22 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
 
 from sqlalchemy.orm import Session
-
 from db_config.sqlalchemy_connect import sess_db
 from models.data.sqlalchemy_models import Signup, Login
 from repository.signup import SignupRepository
 from repository.login import LoginRepository
 
+from fastapi.security import HTTPBasicCredentials
+from security.secure import authenticate, get_password_hash, http_basic
 
-from security.secure import authenticate, get_password_hash
 from datetime import date
 router = APIRouter()
 
-@router.get("/approve/signup", dependencies=[Depends(authenticate)])
-def signup_approve(username:str, sess:Session = Depends(sess_db)): 
+@router.get("/approve/signup")
+def signup_approve(username:str, credentials: HTTPBasicCredentials = Depends(http_basic), sess:Session = Depends(sess_db)): 
     signuprepo = SignupRepository(sess)
     result:Signup = signuprepo.get_signup_username(username) 
     print(result)
@@ -32,12 +32,30 @@ def signup_approve(username:str, sess:Session = Depends(sess_db)):
         else:
             return login
         
-@router.get("/login", dependencies=[Depends(authenticate)])
-def login(sess:Session = Depends(sess_db)):
-    return {"success": "true"}
+@router.get("/login")
+def login(credentials: HTTPBasicCredentials = Depends(http_basic), sess:Session = Depends(sess_db)):
+    
+    loginrepo = LoginRepository(sess)
+    account = loginrepo.get_all_login_username(credentials.username)
+    if authenticate(credentials, account) and not account == None:
+        return account
+    else:
+        raise HTTPException(
+            status_code=400, detail="Incorrect username or password")
+
+@router.delete("/login/delete/{id}")
+def delete_login(id:int, credentials: HTTPBasicCredentials = Depends(http_basic), sess:Session = Depends(sess_db)):
+    
+    loginrepo = LoginRepository(sess)
+    result = loginrepo.delete_login(id)
+    if result == True:
+        return JSONResponse(content={'message':'login deleted successfully'}, status_code=201)  
+    else:
+        raise HTTPException(
+            status_code=400, detail="Incorrect username or password")
         
-@router.get("/login/users/list", dependencies=[Depends(authenticate)])
-def list_all_login(sess:Session = Depends(sess_db)):
+@router.get("/login/users/list")
+def list_all_login(credentials: HTTPBasicCredentials = Depends(http_basic), sess:Session = Depends(sess_db)):
     loginrepo = LoginRepository(sess)
     users = loginrepo.get_all_login()
     return jsonable_encoder(users)
