f26dc1568962583cf0192e1dae707f1b9a84ac29
sjctrags <sjctrags@yahoo.com>
2022-09-09 09:22:39 +0800

    Fixed some lines.
    Fixed some bugs.

--- ch09/models/data/ratings_enum.py
+++ ch09/models/data/ratings_enum.py
@@ -1,13 +1,13 @@
 from enum import Enum
 
-class FoodRatingScale(Enum):
+class FoodRatingScale(int, Enum):
     FIVE_STAR = 5
     FOUR_STAR = 4
     THREE_STAR = 3
     TWO_STAR = 2
     ONE_STAR = 1
     
-class AmbianceRatingScale(Enum):
+class AmbianceRatingScale(int, Enum):
      STRONGLY_AGREE = 7
      AGREE = 6
      SOMEWHAT_AGREE = 5
